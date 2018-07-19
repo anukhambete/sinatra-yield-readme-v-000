5fd8d061d9af97bbb8fc13c347a6ff639f47a1a7
Anu Khambete <anukhambete@gmail.com>
2018-07-19 22:37:18 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,15 +6,5 @@ class App < Sinatra::Base
     erb :newteam
   end
 
-  post '/newteam' do
-    @name = params[:name]
-    @coach = params[:coach]
-    @pg = params[:pg]
-    @sg = params[:sg]
-    @pf = params[:pf]
-    @sf = params[:sf]
-    @c = params[:c]
-    erb:team
-  end
 
 end
